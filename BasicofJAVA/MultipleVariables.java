9ab7074d1509d4b0c9bf39314b0bf11fcd3038d4
Suryansh rana <suryansh2nd@gmail.com>
2024-12-06 18:22:17 +0530

    my first Program on GitHub

---
+++ BasicofJAVA/MultipleVariables.java
@@ -0,0 +1,17 @@
+package com.suryansh.BasicofJAVA;
+
+public class MultipleVariables {
+    public static void main(String[] args) {
+        int x =6, y = 9 , z= 5;
+        System.out.println("The value of x "+x);
+        System.out.println("The value of y "+y);
+
+        z= x+y-z;
+        z = x/z+30;
+        System.out.println("The value of z "+z);
+        System.out.println(x+y+z);
+        System.out.println("print a , y , z value "+x+y+z);
+
+
+    }
+}
---
+++ BasicofJAVA/VARIABLEint.java
@@ -0,0 +1,19 @@
+package com.suryansh.BasicofJAVA;
+
+public class VARIABLEint {
+    public static void main(String[] args) {
+        int x;
+        x=500;
+        System.out.println(x);
+        x=45;
+        x=4-6+2/3*6;
+        System.out.println(x);
+
+        int a = 6 , b = 8 ;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a/b);
+        System.out.println(a*b);
+
+    }
+}
