9caed3ac0dae86a1d8933231047a5c0e86b1dada
Suryansh rana <suryansh2nd@gmail.com>
2024-12-10 19:49:30 +0530

    Taking Input to User program

---
+++ BasicofJAVA/Basic.java
@@ -0,0 +1,11 @@
+package com.suryansh.BasicofJAVA;
+
+public class Basic {
+    public static void main(String[] args) {
+        System.out.println(" hello suryansh");
+        System.out.println(" hello every one ");
+        System.out.println("i will become next good developer and programmer ");
+        System.out.println("i am enginear");
+    }
+    
+}
---
+++ BasicofJAVA/PrintingNumbers.java
@@ -0,0 +1,12 @@
+package com.suryansh.BasicofJAVA;
+
+public class PrintingNumbers {
+    public static void main(String[] args) {
+        System.out.println(4+5+8);// consider as Numbers
+        System.out.println("4+5+6");// consider as a text
+       // System.out.println(hello world);
+        System.out.println("hi how about you");
+        System.out.println(6*7/4);
+        System.out.println();
+    }
+}
